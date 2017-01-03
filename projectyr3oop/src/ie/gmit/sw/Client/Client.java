ab7c13e4a8682095ee14079fb88fbd02b59e15f4
Alan Doyle(G00328071) <doyleaa1@gmail.com>
2017-01-03 15:58:43 +0000

    First Commit

---
+++ projectyr3oop/src/ie/gmit/sw/Client/Client.java
@@ -0,0 +1,12 @@
+package ie.gmit.sw.Client;
+
+import java.util.Scanner;
+
+public class Client {
+
+	public static void main(String[] args) {
+
+		Scanner console = new Scanner(System.in);
+	}
+
+}// class
